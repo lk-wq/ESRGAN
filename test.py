ceca4561fee598252a4361f4eb32e26a6790dacc
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-01-22 11:22:57 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 import os.path as osp
 import glob
-import cv2
+# import cv2
+import PIL
+from PIL import Image
 import numpy as np
 import torch
 import RRDBNet_arch as arch
 
-model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
+model_path = '/content/ESRGAN/models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+device = torch.device('cpu')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
-test_img_folder = 'LR/*'
+test_img_folder = '/content/ESRGAN/LR/*'
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
@@ -24,7 +26,7 @@ for path in glob.glob(test_img_folder):
     base = osp.splitext(osp.basename(path))[0]
     print(idx, base)
     # read images
-    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    img = np.asarray(Image.open(path))
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
@@ -33,5 +35,7 @@ for path in glob.glob(test_img_folder):
     with torch.no_grad():
         output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-    output = (output * 255.0).round()
-    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    output = np.uint8((output * 255.0).round())
+    print(type(output))
+    output = Image.fromarray(output)
+    output.save('/content/ESRGAN/results/{:s}_rlt.png'.format(base))
